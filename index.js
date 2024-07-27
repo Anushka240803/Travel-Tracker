af9fd82b6223585cb3e6b28c840d504e28ad9307
Ujjwal <100501442+o0PHOENIX0o@users.noreply.github.com>
2024-07-27 22:20:14 +0530

    fixed index.js

--- index.js
+++ index.js
@@ -11,52 +11,50 @@ const db = new pg.Client({
 });
 
 db.connect();
+
 const app = express();
 const port = 3000;
-let count=[];
-let count2=[];
-var countries=[];
-// var i= 6;
+let total;
+let country_codes=[];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-function visited(){
-  db.query("SELECT country FROM vcountries",(err,res)=>{
-    if(err){
-      console.error("Error executing query ",err.stack);
-      res.status(500).json(err);
-    }else{
-      count=res.rows;
-    }
-  });
+async function visited(){
+  try{
+    const response = await db.query("SELECT country FROM vcountries");
+    total = response.rows.length;
+    response.rows.forEach((data) => {
+      country_codes.push(data.country)
+    });
+  }catch (error){
+    console.error("Error executing query ",err.stack);
+    res.status(500).json(err);
+  }
 };
-visited();
+
+
+
 app.get("/", async (req, res) => {
-  //Write your code here.
-  
-  count.forEach((code)=>{
-    if(!countries.includes(code.country)){
-      countries.push(code.country);
-      console.log(code);
-    }
-  });
-  res.render("index.ejs",{total:countries.length,
-    countries:countries,
-  });
+  await visited();
+  res.render("index.ejs",{total:total, countries:country_codes});
 });
-var c;
+
+
 app.post("/add", async(req,res)=>{
-  const result=await db.query("SELECT country_code FROM countries WHERE country_name = $1",[req.body["country"]]);
+  let newCountry = req.body.country.toLowerCase();
+  console.log(newCountry);
+
+  const result=await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) = $1",[newCountry]);
   console.log(result.rows);
   if(result.rows.length==1){
-    db.query("INSERT INTO vcountries (country) VALUES ($1)",[result.rows[0].country_code]);
-    
+    let newCountryCode = result.rows[0].country_code;
+    await db.query("INSERT INTO vcountries (country) VALUES ($1)",[newCountryCode]);
   }
-  visited();
   res.redirect("/");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  
   console.log(`Server running on http://localhost:${port}`);
 });
